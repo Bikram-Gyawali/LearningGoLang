9136e6590db2343a9648bf25054ce450e4ed9512
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-03-14 14:48:59 +0545

    variables in golang and basic operation

---
+++ day01/variablesInGo.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var val1=10
+	var val2=20
+	var sum=val1+val2;
+	var multiplication=val1*val2
+	var div=val2/val1
+	fmt.Println(sum,multiplication,div)
+}
