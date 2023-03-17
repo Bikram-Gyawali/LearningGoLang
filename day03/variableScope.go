5597f8be541f15b5ae8fba9396278d2a41fbee21
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-03-17 14:08:57 +0545

    variables scope in go

--- day03/variableScope.go
+++ day03/variableScope.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
+var b=15
+var a=11   // we call it package level variable
 
 func calc(){
-	var a = 10
-	fmt.Println(a)
+	var a = 10 // we call it function level variable
+	fmt.Println("in calc local",a) 
 }
 
 func main(){
 	calc()
-	fmt.Println()
+	// fmt.Println(a) -->  returns undefined a error
+	fmt.Println("in main a",a);
+	fmt.Println(b)
 }
