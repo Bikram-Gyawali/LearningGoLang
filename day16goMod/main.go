24b9fc26c0e1f73206e2c16ec94c73cb12435b75
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-04-02 23:17:57 +0545

    golang web get request api server

---
+++ day16goMod/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("heello ")
+
+	greet()
+	r := mux.NewRouter()
+	r.HandleFunc("/", servHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":3000", r)) 
+
+}
+
+func greet() {
+	fmt.Println("hey there goodmorning")
+}
+
+func servHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to golang home</h1>"))
+}
