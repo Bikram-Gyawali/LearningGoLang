66f51c6a9b46beb586ebeea9bb72cde9aac47996
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-04-05 16:23:47 +0545

    cru op delete remaining

--- day17/main.go
+++ day17/main.go
@@ -142,19 +142,4 @@ func updateCourse(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("delete one  course")
-	w.Header().Set("Content-Type", "application/json")
 
-	params := mux.Vars(r)
-
-	for index, course := range courses {
-		if course.CourseId == params["id"] {
-			courses = append(courses[:index], courses[index+1]...)
-			break
-		}
-	}
-
-}
-
-// routing
