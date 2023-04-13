0ee4932b8fd077ba070ac13634b3e4c8c5ecf878
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-04-14 00:46:16 +0545

    read line by line

--- day23_ReadFiles/main.go
+++ day23_ReadFiles/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -24,10 +26,38 @@ func ReadStats(filename string) {
 	fmt.Printf("Filee Name: %s\n", stats.Name())
 
 	fmt.Printf("Time modified:  %v\n", stats.ModTime().Format("15:04:03"))
+
+}
+
+func ReadWholeFile(filename string) {
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(contents))
+}
+
+func ReadByLine(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer file.Close()
+
+
+	scanner :=bufio.NewScanner(file)
+
+	for scanner.Scan(){
+		
+	}
 }
 
 func main() {
 	filename := "file.dat"
 
-	ReadStats(filename)
+	// ReadStats(filename)
+	ReadWholeFile(filename)
 }
