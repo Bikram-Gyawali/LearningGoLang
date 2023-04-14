6f249b404061549e3cf1d3bb49822447af107f0b
Bikram-Gyawali <bikramgyawali57@gmail.com>
2023-04-15 00:38:48 +0545

    file reading with golang

---
+++ day23_ReadFiles/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func ReadStats(filename string) {
+	file, err := os.Open((filename))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer file.Close()
+
+	stats, err := file.Stat()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Filee Name: %s\n", stats.Name())
+
+	fmt.Printf("Time modified:  %v\n", stats.ModTime().Format("15:04:03"))
+}
+
+func main() {
+	filename := "file.dat"
+	ReadStats(filename)
+}
